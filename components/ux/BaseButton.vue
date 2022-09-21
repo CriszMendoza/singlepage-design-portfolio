70a9232d5c1eecf818708717401f99ead0e586a2
Crisz Mendoza <criszmendoza212@gmail.com>
2022-09-21 10:49:30 -0500

    Componente base-button/listo

---
+++ components/ux/BaseButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button @click="submit" :class="type">{{title}}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            require: true
+        },
+        type: {
+            type: String,
+            require: true
+        }
+    },
+    methods: {
+        submit(){
+            this.$emit('submitButton')
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '~/assets/scss/main';
+
+    button{
+        width: 228px;
+        height: 56px;
+        border: none;
+        border-radius: 28px;
+        cursor: pointer;
+        color: $light-cream;
+        @include font-button;
+    }
+
+    .principal{
+        background-color: $black;
+        &:hover{
+            background-color: $galactic-blue;
+        }
+    }
+
+    .secondary{
+        background-color: $light-red;
+        &:hover{
+            background-color: $summer-yellow;
+        }
+    }
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,18 @@
+export default{
+    head: {
+        title: 'Frontend Mentor | Single-page design portfolio',
+        link: [
+            {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"},
+            {rel:"preconnect", href:"https://fonts.googleapis.com"},
+            {rel:"preconnect", href:"https://fonts.gstatic.com", crossorigin:true},
+            {href:"https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;700&display=swap", rel:"stylesheet"}
+        ],
+        meta: [
+            {charset:"UTF-8"},
+            {name:"viewport", content:"width=device-width, initial-scale=1.0"}
+        ]
+    },
+    css: [
+        '~assets/scss/main.scss',
+    ],
+}
